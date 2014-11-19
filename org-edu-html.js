7732e439507b4ff1fce6e2593ec0b6714de27d81
Marcin Borkowski <mbork@mbork.pl>
2014-11-19 23:54:36 +0100

    Add basic support for multiple choice tests

--- org-edu-html.js
+++ org-edu-html.js
@@ -19,6 +19,25 @@ $( document ).ready(function() {
 	    $(this).parent().children().filter('div.wrong').show();
 	};
     });
+    $("fieldset.mct").append(
+	'<button type="button" class="mct-check">Check</button>',
+	'<div class="ok" style="cursor:default" hidden>OK!</div>',
+	'<div class="wrong style="cursor:default" hidden>Wrong...</div>');
+    $('button.mct-check').click(function() {
+    	var somethingIsWrong = false;
+    	$(this).parent().children('div').children('input').each(function() {
+    	    if(($(this).attr('value') == 1) != $(this).is(':checked')) {
+    	    	somethingIsWrong = true;
+    	    }
+    	});
+    	if(!somethingIsWrong) {
+    	    OkWrongSiblings($(this)).hide();
+    	    $(this).parent().children().filter('div.ok').show();
+    	} else {
+    	    OkWrongSiblings($(this)).hide();
+    	    $(this).parent().children().filter('div.wrong').show();
+    	}
+    });
     $('div.ok,div.wrong').click(function () {
 	$(this).hide();
     });
