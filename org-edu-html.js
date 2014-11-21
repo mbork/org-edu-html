a255f4a24484271bf3d809aa9fed3ddd2a9b73d4
Marcin Borkowski <mbork@mbork.pl>
2014-11-21 22:34:36 +0100

    Add comments on right/wrong answers in MCT

--- org-edu-html.js
+++ org-edu-html.js
@@ -9,27 +9,32 @@ $( document ).ready(function() {
     $("fieldset.sct").append(
 	'<button type="button" class="sct-check">Check</button>',
 	'<div class="ok" style="cursor:default" hidden>OK!</div>',
-	'<div class="wrong style="cursor:default" hidden>Wrong...</div>');
+	'<div class="wrong" style="cursor:default" hidden>Wrong...</div>');
     $('button.sct-check').click(function() {
 	if($(this).parent().children('div').children('input:checked').attr('value') == 1) {
 	    OkWrongSiblings($(this)).hide();
-	    $(this).parent().children().filter('div.ok').show();
+	    $(this).parent().children().filter('div.ok,div.comment_ok').show();
 	} else {
 	    OkWrongSiblings($(this)).hide();
-	    $(this).parent().children().filter('div.wrong').show();
+	    $(this).parent().children().filter('div.wrong,div.comment_wrong').show();
 	};
     });
     $("fieldset.mct").append(
 	'<button type="button" class="mct-check">Check</button>',
 	'<div class="ok" style="cursor:default" hidden>OK!</div>',
-	'<div class="wrong style="cursor:default" hidden>Wrong...</div>');
+	'<div class="wrong" style="cursor:default" hidden>Wrong...</div>');
     $('button.mct-check').click(function() {
+	$(this).parent().find('div.ok,div.wrong,div.comment_ok,div.comment_wrong').hide()
     	var somethingIsWrong = false;
     	$(this).parent().children('div').children('input').each(function() {
     	    if(($(this).attr('value') == 1) != $(this).is(':checked')) {
     	    	somethingIsWrong = true;
-    	    }
+		$(this).parent().find('div.comment_wrong').show();
+    	    } else {
+		$(this).parent().find('div.comment_ok').show();
+	    }
     	});
+	// $(this).parent().find('div.comment_ok,div.comment_wrong').show(); // źle!!!
     	if(!somethingIsWrong) {
     	    OkWrongSiblings($(this)).hide();
     	    $(this).parent().children().filter('div.ok').show();
@@ -38,7 +43,7 @@ $( document ).ready(function() {
     	    $(this).parent().children().filter('div.wrong').show();
     	}
     });
-    $('div.ok,div.wrong').click(function () {
+    $('div.ok,div.wrong,div.comment_ok,div.comment_wrong').hide().click(function () {
 	$(this).hide();
     });
 });
